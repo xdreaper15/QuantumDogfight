89ac649482daf2a4d298dd0c4d60ab6a1b2d3a05
Tony Newberry <newberry.anthony@gmail.com>
2018-01-22 01:17:48 -0500

    Quantum Spawner RotationTry

--- Assets/Scripts/QuantumSpawner.cs
+++ Assets/Scripts/QuantumSpawner.cs
@@ -11,21 +11,25 @@ public class QuantumSpawner : MonoBehaviour
 	public Transform Asteroid;
 	public GameObject WarningField;
 	public GameObject AsteroidClone;
+	public Rigidbody AstRigid;
 	public int maxAst = 10; //max # of Asteroids
 	public int currentAst = 0; //current # of Asteroids
 	public int currentWarning = 0; //current # of Asteroids
 	public int minAstSize = 100; //min Ast size
 	public int maxAstSize = 150; //max Ast size
-	public int maxSpawnRadius = 50; //max radius at which Asteroids spawn
+	public int maxAstTorq = 100; //max Ast Torq
+	public const int maxSpawnRadius = 50; //max radius at which Asteroids spawn
 	private Quaternion astRotation;
 	private Vector3 astSpawn;
 	private float warningSize;
 	private float astSize;
 	private float astWarnBuffer = 1.08F;
+	private int astTorqX;
+	private int astTorqY;
+	private int astTorqZ;
 	private int spawnLocX;
 	private int spawnLocY;
 	private int spawnLocZ;
-	private int arg1;
 
 	// Use this for initialization
 	void Start ()
@@ -43,45 +47,48 @@ public class QuantumSpawner : MonoBehaviour
 		{
 			if (currentWarning < currentAst + 1) 
 			{
-				SpawnWarning ("test");
+				SpawnWarning ();
 				currentWarning = currentWarning + 1;
 			}
 		}
 	}
 
-	void SpawnWarning (string arg1)
+	void SpawnWarning ()
 	{
-		arg1 = "Warning Zone: " + currentAst;
 		astSize = Random.Range(minAstSize, maxAstSize); //establish ASTEROID radius
-		astRotation.Set(7, 15, 22, 3);
+
+		astRotation.Set(7, 15, 22, 3); //Sets an initial Asteroid Rotation (Should be randomized in the future)
+
 		warningSize = astSize * astWarnBuffer;
 
-		spawnLocX = Random.Range (-50, 50); //spawn at random X position
-		spawnLocY = Random.Range (-50, 50); //spawn at random Y position
-		spawnLocZ = Random.Range (-50, 50); //spawn at random Z position
+		spawnLocX = Random.Range (-maxSpawnRadius, maxSpawnRadius); //spawn at random X position
+		spawnLocY = Random.Range (-maxSpawnRadius, maxSpawnRadius); //spawn at random Y position
+		spawnLocZ = Random.Range (-maxSpawnRadius, maxSpawnRadius); //spawn at random Z position
 
 		astSpawn.Set (spawnLocX, spawnLocY, spawnLocZ); //randomly assigns ASTEROID spawn position
 
 		Warning.localScale = new Vector3 (warningSize, warningSize, warningSize); // sets the size of the WARNING
 		Asteroid.localScale = new Vector3 (astSize, astSize, astSize); //sets the size of the ASTEROID
 
-		GameObject clone = (GameObject)Instantiate (WarningField, astSpawn, transform.rotation);
-		//Instantiate (Warning, astSpawn, transform.rotation); // spawns WARNING
-		Destroy(clone, 2.2f);
+		GameObject clone = (GameObject)Instantiate (WarningField, astSpawn, transform.rotation); //Spawns Warning Clone
+		Destroy(clone, 2.2f); //Destroys Warning Clone
 		Invoke ("SpawnAsteroid", 2);
 
 	}
 
 	void SpawnAsteroid()
 	{
-		GameObject astClone = (GameObject)Instantiate (AsteroidClone, astSpawn, astRotation);
-//		GameObject astClone = (GameObject)Instantiate (Asteroid, astSpawn, transform.rotation); //spawn ASTEROID
-		Destroy(astClone, 20f);
-		currentAst++;
+		GameObject astClone = (GameObject)Instantiate (AsteroidClone, astSpawn, astRotation); // Spawn Asteroid Clone
+
+		astTorqX = Random.Range (-maxAstTorq, maxAstTorq); //establish random X-axis Torque
+		astTorqY = Random.Range (-maxAstTorq, maxAstTorq); //establish random Y-axis Torque
+		astTorqZ = Random.Range (-maxAstTorq, maxAstTorq); //establish random Z-axis Torque
+		AstRigid.AddRelativeTorque (transform.forward * astTorqX); //Apply established Torques
+		AstRigid.AddRelativeTorque (transform.up * astTorqY); //Apply established Torques
+		AstRigid.AddRelativeTorque (transform.right * astTorqZ); //Apply established Torques
+
+		Destroy(astClone, 20f); //Destroy Asteroid Clone
+		currentAst++; // Adds 1 to Asteroid Number
 	}
-		
+
 }
-
-
-
-
